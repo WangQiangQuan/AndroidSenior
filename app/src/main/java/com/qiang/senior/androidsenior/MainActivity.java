4aede59530eec8cfc6357d4dba6780e3c9aa9658
wangq <1281550584@qq.com>
2017-02-08 15:26:05 +0800

    事件分发

--- app/src/main/java/com/qiang/senior/androidsenior/MainActivity.java
+++ app/src/main/java/com/qiang/senior/androidsenior/MainActivity.java
@@ -1,7 +1,10 @@
 package com.qiang.senior.androidsenior;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.qiang.senior.androidsenior.chapter1.DispatchEventActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent mIntent = new Intent(this, DispatchEventActivity.class);
+        this.startActivity(mIntent);
     }
 }
